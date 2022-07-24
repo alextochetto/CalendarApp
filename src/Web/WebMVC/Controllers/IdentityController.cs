7c785255eb1963eb031fe25c72f3a5c1cb2503e8
Alex Tochetto <alextochetto@gmail.com>
2022-07-24 20:27:43 -0300

    change _layout

--- src/Web/WebMVC/Controllers/IdentityController.cs
+++ src/Web/WebMVC/Controllers/IdentityController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebMVC.Models;
 
 namespace WebMVC.Controllers
 {
+    [AllowAnonymous]
     public class IdentityController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login(IdentityLoginUserViewModel identityLoginUserViewModel)
+        {
+            return RedirectToAction("Dashboard", "Home");
         }
     }
 }
--- src/Web/WebMVC/Models/IdentityLoginUserViewModel.cs
+++ src/Web/WebMVC/Models/IdentityLoginUserViewModel.cs
@@ -5,11 +5,11 @@ namespace WebMVC.Models
 {
     public class IdentityLoginUserViewModel
     {
-        [Required(ErrorMessage = "Obrigatório informar o {0}")]
+        [Required(ErrorMessage = "Obrigatório informar o E-mail")]
         [DataType(DataType.EmailAddress, ErrorMessage = "E-mail inválido")]
         [Display(Description = "E-mail", Prompt = "Informe seu E-mail")]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Obrigatório informar a {0}")]
+        [Required(ErrorMessage = "Obrigatório informar a Senha")]
         [DataType(DataType.Password)]
         [Display(Description = "Senha", Prompt = "Informe sua Senha")]
         public string Password { get; set; }
