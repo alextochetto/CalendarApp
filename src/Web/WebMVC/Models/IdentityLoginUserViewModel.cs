7a95e1d720feceecba6967d0985d81432d72379c
kaue yorinori souza <kaueys@outlook.com>
2022-09-06 21:10:26 -0300

    select2

--- src/Web/WebMVC/Models/IdentityLoginUserViewModel.cs
+++ src/Web/WebMVC/Models/IdentityLoginUserViewModel.cs
@@ -9,6 +9,8 @@ namespace WebMVC.Models
         [DataType(DataType.EmailAddress, ErrorMessage = "E-mail inválido")]
         [Display(Description = "E-mail", Prompt = "Informe seu E-mail")]
         public string Email { get; set; }
+
+
         [Required(ErrorMessage = "Obrigatório informar a Senha")]
         [DataType(DataType.Password)]
         [Display(Description = "Senha", Prompt = "Informe sua Senha")]
